16844830015cc3f3d576589b65f3cdbc745a1129
Rafael Saca <raffaellsaca@gmail.com>
2024-03-26 21:38:21 -0300

    feat: Rota para tela de relação de funcionarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'funcionarios' },
+  {
+    path: 'funcionarios',
+    loadChildren: () =>
+      import('./funcionarios/funcionarios.module').then(
+        (m) => m.FuncionariosModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
